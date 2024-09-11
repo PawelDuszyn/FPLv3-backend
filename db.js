9571a8d4334d4da3b2d7e7333099632c5018b69f
pawelduszyn <duszyn98@gmail.com>
2024-09-11 09:04:19 +0200

    connecting db

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "FPL",
-    password: "Moa0d9aMnw",
-    port: 5432,
-  });
+  connectionString: process.env.DATABASE_URL || 'postgresql://fpl_ppj0_user:pVVJT6yzEllocA418PPJa8QrhS2roRQw@dpg-crgagprv2p9s73aaeql0-a.frankfurt-postgres.render.com/fpl_ppj0',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-module.exports = pool;
+pool.connect((err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych', err);
+  } else {
+    console.log('Połączono z bazą danych');
+  }
+});
